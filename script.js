11b0f5a48a383d8be4a41b8c496b788d2532f933
edith.githinji <edithgithinji2020@gmail.com>
2024-12-07 13:39:11 +0300

    adding ittems to the container div using the querry selector.

--- script.js
+++ script.js
@@ -2,4 +2,13 @@ document.querySelector("#addBtn").onclick = function() {
      if(document.querySelector("#new-item input").value.length == 0){
         alert("Please enter an item.")
      }
+     else {
+        document.querySelector('#items').innerHTML += 
+        `
+        <div class = "item"> 
+            <span id= "item-name"> </span>
+            ${document.querySelector("#new-item input").value}
+        </div>
+        `
+     }
 }
