25e0a7219322fa19b3fc5a7d86fc39a7a4f841ee
Edith Githinji <152801726+Edith123321@users.noreply.github.com>
2024-12-13 16:21:00 +0300

    adding an array and more event handlers

--- script.js
+++ script.js
@@ -1,39 +1,59 @@
-document.querySelector("#addBtn").onclick = function() {
-     if(document.querySelector("#new-item input").value.length == 0){
-        alert("Please enter an item.")
-     }
-     else {
-        document.querySelector('#items').innerHTML += 
-        `
-        <div class = "item"> 
-            <span id= "item-name">
-            ${document.querySelector("#new-item input").value} 
+// Array to store shopping list items
+let shoppingList = [];
+
+document.querySelector("#addBtn").onclick = function () {
+    const newItemInput = document.querySelector("#new-item input");
+    const newItem = newItemInput.value.trim();
+
+    if (newItem.length === 0) {
+        alert("Please enter an item.");
+    } else {
+        // Add item to the array
+        shoppingList.push(newItem);
+
+        // Add item to the DOM
+        document.querySelector("#items").innerHTML += `
+        <div class="item"> 
+            <span id="item-name">
+                ${newItem}
             </span>
-            <button class = "delete">
-             <i class="fa fa-trash"></i>
+            <button class="delete">
+                <i class="fa fa-trash"></i>
             </button>
         </div>
         `;
 
+        // Add delete functionality
         var current_items = document.querySelectorAll(".delete");
-        for(var i=0; i<current_items.length; i++) {
-            current_items[i].onclick = function(){
+        for (var i = 0; i < current_items.length; i++) {
+            current_items[i].onclick = function () {
+                const itemName = this.previousElementSibling.textContent.trim();
+                // Remove item from array
+                shoppingList = shoppingList.filter(item => item !== itemName);
+
+                // Remove item from DOM
                 this.parentNode.remove();
-            }
+            };
         }
 
+        // Add purchased toggle functionality
         var items = document.querySelectorAll(".item");
-        for(var i=0; i<items.length; i++) {
-            items[i].onclick = function() {
-                this.classList.toggle('bought');
-            }
+        for (var i = 0; i < items.length; i++) {
+            items[i].onclick = function () {
+                this.classList.toggle("bought");
+            };
         }
-        // Add functionality to clear all items in the list
-         document.querySelector("#clearBtn").onclick = function() {
-         // Clear the entire items container
-          document.querySelector("#items").innerHTML = "";
-         }
 
-        document.querySelector("#new-item input").value = "";
-     }
-}
+        // Add functionality to clear all items
+        document.querySelector("#clearBtn").onclick = function () {
+            // Clear the array
+            shoppingList = [];
+
+            // Clear the DOM
+            document.querySelector("#items").innerHTML = "";
+        };
+
+        // Clear the input field
+        newItemInput.value = "";
+    }
+};
